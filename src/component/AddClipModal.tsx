6255d47ae50a0221b6ecaf362c18b8a0074ccf09
LikelyLime <qorwlgml2003@naver.com>
2026-05-22 15:31:37 +0900

    get연습

--- src/component/AddClipModal.tsx
+++ src/component/AddClipModal.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 interface OwnProps{
     onOffModal: () => void
 }
 
 function AddClipModal({onOffModal}: OwnProps){
-    
+    const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+    const [inputKey, setInputKey] = useState<string>();
+
+
     return(
         <div className="Overlay">
             
                 <div className="Modal">
                     <h3 className="modalTitle">클립 추가</h3>
-                    <input className="modalInput" placeholder="URL"></input>
+                    <input className="modalInput" placeholder="URL" onChange={(a) => {setInputKey(a)}}></input>
                     <br/>
                     <div className="modalButtons">
                         <button className="modalCloseButton" onClick={onOffModal}>닫기</button>
-                        <button className="modalPlusButton">추가</button>
+                        <button className="modalPlusButton" onClick={(a)=>{
+                            debugger;
+                            axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${a}&key=${apiKey}`)
+                            .then(response => {
+
+                            })
+                            .catch(error => {
+
+                            })
+                        }}>추가</button>
                     </div>
                 </div>
             
